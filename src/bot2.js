de17ed36f902399a9592bd8a787c460e1996372c
Phet11234 <phettest03@gmail.com>
2017-10-29 11:59:18 +0700

    Rename bot.js to bot2.js

---
+++ src/bot2.js
@@ -0,0 +1,26 @@
+const LineConnect = require('./connect');
+let LINE = require('./main.js');
+
+const auth = {
+	authToken: ' Em7hDLcPkCAT7A2Yql06.5EmrTQ2t7zCnEmN8osfbTG.moXqIveI6MSxRvUoyVFZMzqgCV9Jbin6pNyR1n73JVE=',
+	certificate: 'cert here',
+}
+// let client =  new LineConnect(auth);
+let client =  new LineConnect();
+
+client.startx().then(async (res) => {
+	
+	while(true) {
+		try {
+			ops = await client.fetchOps(res.operation.revision);
+		} catch(error) {
+			console.log('error',error)
+		}
+		for (let op in ops) {
+			if(ops[op].revision.toString() != -1){
+				res.operation.revision = ops[op].revision;
+				LINE.poll(ops[op])
+			}
+		}
+	}
+});
