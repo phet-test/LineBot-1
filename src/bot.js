0eba630bb0e394bbf883de385953e0e00a91cfbb
Phet11234 <phettest03@gmail.com>
2017-10-29 16:17:19 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,8 @@ const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
 const auth = {
-	authToken: ' token disini',
-	certificate: 'cert here',
+	authToken: ' Emc1TmkexDaAdn3DfS2a.apGNZMzba418M2CeQxD9/G.TOfUUo98+oW/iD8ML+Uasf4F1uwIg64JJ3ouWXrTJgg=',
+	certificate: '211a96a151b17c3d3e9f00a8f5b60aae59d34ea4f574fef07808e760275507e6',
 }
 // let client =  new LineConnect(auth);
 let client =  new LineConnect();
